0b38f9a2772577ab66965ac19d04f89a39bffa7e
daliarojas <kyloren4891@gmail.com>
2024-03-03 22:48:11 -0700

    prep1 complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask the user to enter their name
+        Console.Write("What is your first name? ");
+ 
+        // Read the input from the user
+        string firstname = Console.ReadLine();
+ 
+        // Ask the user to enter their name
+        Console.Write("What is your last name? ");
+ 
+        // Read the input from the user
+        string lastname = Console.ReadLine();
+ 
+        Console.WriteLine("Your name is " + lastname + ", " + firstname + " " + lastname + "." );
     }
 }
