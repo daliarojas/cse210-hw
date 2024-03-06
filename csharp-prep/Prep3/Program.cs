8b035d98057f1a472eb3c8165e99aba43a6ff3a8
daliarojas <kyloren4891@gmail.com>
2024-03-06 16:26:35 -0700

    prep 2, 3, 4 and 5 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        {
+            // Generate random number between 1 and 100
+            Random randomGenerator = new Random();
+            int magicNumber = new Random().Next(1, 101);
+
+            int guess = -1;
+            
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int. Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+            } 
+
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
+    
